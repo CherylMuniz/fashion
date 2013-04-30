2f7f634924afc71dd5d079621d623626e5b5050e
Fashion Eye Wear Production <yfedoruck@gmail.com>
2013-04-30 13:30:42 +0100

    fixes

--- app/code/local/Oberig/Fashion/Helper/Data.php
+++ app/code/local/Oberig/Fashion/Helper/Data.php
@@ -4,7 +4,7 @@ class Oberig_Fashion_Helper_Data extends Mage_Core_Helper_Abstract
 	public function isLiveSite()
 	{
 		return 
-			   Mage::app()->getRequest()->getServer('SERVER_NAME') == 'demo.fashioneyewear.co.uk'
-			|| Mage::app()->getRequest()->getServer('SERVER_NAME') == 'www.demo.fashioneyewear.co.uk';
+			   Mage::app()->getRequest()->getServer('SERVER_NAME') == 'fashioneyewear.co.uk'
+			|| Mage::app()->getRequest()->getServer('SERVER_NAME') == 'www.fashioneyewear.co.uk';
 	}
 }
